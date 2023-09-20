7454859ee58377d3477e9660e6246a0f72c1af28
Alex Jercan <jercan_alex27@yahoo.com>
2023-09-20 17:17:15 +0300

    feat: updated multithread and UI

--- slide_twitch/main.py
+++ slide_twitch/main.py
@@ -1,14 +1,14 @@
+""" Twitch Presentation Bot """
 import asyncio
 import concurrent.futures
 import glob
 import json
 import logging
 import os
-import queue
+import random
 import urllib.request
 import wave
-from logging.handlers import QueueHandler, QueueListener
-from typing import Any
+from typing import Dict, Optional
 
 import fakeyou
 import ffmpeg
@@ -16,7 +16,7 @@ import obsws_python as obs
 import openai
 from dotenv import load_dotenv
 from rich.logging import RichHandler
-from tqdm import tqdm
+from rich.progress import Progress
 from twitchAPI import Twitch
 from twitchAPI.chat import Chat, ChatCommand, EventData
 from twitchAPI.oauth import UserAuthenticator
@@ -27,21 +27,19 @@ load_dotenv()
 LOG_LEVEL = os.getenv("APPLICATION_LOG_LEVEL", "DEBUG")
 LOG_FILE = os.getenv("APPLICATION_LOG_FILE", "app.log")
 
-log_queue: queue.Queue[Any] = queue.Queue(-1)
-queue_handler = QueueHandler(log_queue)
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging._nameToLevel[LOG_LEVEL])
-logger.addHandler(queue_handler)
-
 rich_handler = RichHandler()
 file_handler = logging.FileHandler(LOG_FILE)
 file_handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="[%X]",
+    )
 )
 
-queue_listener = QueueListener(log_queue, file_handler, rich_handler)
-queue_listener.start()
+logger = logging.getLogger("slide_twitch")
+logger.setLevel(logging._nameToLevel[LOG_LEVEL])
+logger.addHandler(rich_handler)
+logger.addHandler(file_handler)
 
 SYSTEM = """Your job is to create a slide presentation for a video. \
 In this presentation you must include a speech for the current slide and a \
@@ -113,6 +111,35 @@ def get_output_run(output: str = OUTPUT) -> int:
     os.mkdir(run_path)
 
     return run
+
+
+def get_random_run(output: str = OUTPUT) -> Optional[int]:
+    """Get a random run from the output directory
+
+    Parameters
+    ----------
+    output : str, optional
+        The output directory to use for the files, by default OUTPUT
+
+    Returns
+    -------
+    Optional[int]
+        The run number or None if no runs exist
+    """
+    if not os.path.exists(output):
+        return None
+
+    runs = [
+        int(run)
+        for run in os.listdir(output)
+        if os.path.isdir(os.path.join(output, run))
+        if os.path.exists(os.path.join(output, run, "video.mp4"))
+    ]
+
+    if len(runs) == 0:
+        return None
+
+    return random.choice(runs)
 
 
 def create_video(output: str):
@@ -291,10 +318,110 @@ def create_vtt(output: str):
             current_s = end
 
 
+def create_script(prompt: str, output: str) -> Dict:
+    """Create the script for the presentation
+
+    The script will be saved in the output directory as `presentation.json`.
+    The script will be created by using the system prompt (from config) and
+    the user prompt.
+
+    Parameters
+    ----------
+    prompt : str
+        The user prompt to use
+    output : str
+        The output directory to use for the files
+
+    Returns
+    -------
+    Dict
+        The presentation script
+    """
+    response = openai.ChatCompletion.create(
+        api_key=OPENAI_API_KEY,
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": SYSTEM,
+            },
+            {"role": "user", "content": prompt},
+        ],
+    )
+
+    presentation = json.loads(response.choices[0].message.content)
+    for slide in presentation:
+        slide["speaker"] = SPEAKER
+
+    with open(
+        os.path.join(output, "presentation.json"), "w", encoding="utf-8"
+    ) as file:
+        json.dump(presentation, file, indent=2)
+
+    return presentation
+
+
+def create_image(index: int, slide: Dict, output: str):
+    """Create the image for the slide
+
+    The image will be saved in the output directory as `slide_*.png`. The image
+    will be created by using the image prompt from the slide.
+
+    Parameters
+    ----------
+    index : int
+        The slide index
+    slide : Dict
+        The slide to create the image for
+    output : str
+        The output directory to use for the files
+    """
+    response = openai.Image.create(
+        prompt=slide["image"],
+        n=1,
+        size="1024x1024",
+        api_key=OPENAI_API_KEY,
+    )
+    image_url = response["data"][0]["url"]
+
+    path = os.path.join(output, f"slide_{index:02d}.png")
+    urllib.request.urlretrieve(image_url, path)
+
+    logger.debug(f"Slide {index}: Image '{slide['image']}'")
+
+
+def create_audio(index: int, slide: Dict, output: str):
+    """Create the audio for the slide
+
+    The audio will be saved in the output directory as `slide_*.wav`. The audio
+    will be created by using the text prompt from the slide.
+
+    Parameters
+    ----------
+    index : int
+        The slide index
+    slide : Dict
+        The slide to create the audio for
+    output : str
+        The output directory to use for the files
+    """
+    fk_you = fakeyou.FakeYou()
+
+    try:
+        fk_you.login(username=FAKEYOU_USERNAME, password=FAKEYOU_PASSWORD)
+    except fakeyou.exception.InvalidCredentials:
+        logger.warning("Invalid login credentials for FakeYou")
+    except fakeyou.exception.TooManyRequests:
+        logger.error("Too many requests for FakeYou")
+
+    path = os.path.join(output, f"slide_{index:02d}.wav")
+    fk_you.say(slide["text"], slide["speaker"]).save(path)
+
+    logger.debug(f"Slide {index}: TTS ({slide['speaker']}) '{slide['text']}'")
+
+
 def create_slides(
     prompt: str,
-    model: str = MODEL,
-    speaker: str = SPEAKER,
     output: str = os.path.curdir,
 ):
     """Create the slides for the presentation
@@ -307,73 +434,44 @@ def create_slides(
     ----------
     prompt : str
         The user prompt to use
-    model : str, optional
-        The model to use, by default c.MODEL
-    speaker : str, optional
-        The speaker to use, by default c.SPEAKER
     output : str, optional
         The output directory to use for the files, by default os.path.curdir
     """
-    logger.debug("Creating slides...")
-
-    fk_you = fakeyou.FakeYou()
-
-    try:
-        fk_you.login(username=FAKEYOU_USERNAME, password=FAKEYOU_PASSWORD)
-    except fakeyou.exception.InvalidCredentials:
-        logger.warning("Invalid login credentials for FakeYou")
-    except fakeyou.exception.TooManyRequests:
-        logger.error("Too many requests for FakeYou")
-
     with open(
         os.path.join(output, "prompt.txt"), "w", encoding="utf-8"
     ) as file:
         file.write(prompt)
 
-    response = openai.ChatCompletion.create(
-        api_key=OPENAI_API_KEY,
-        model=model,
-        messages=[
-            {
-                "role": "system",
-                "content": SYSTEM,
-            },
-            {"role": "user", "content": prompt},
-        ],
-    )
+    with Progress() as progress:
+        _ = progress.add_task("[yellow]Generating script", total=None)
 
-    presentation = json.loads(response.choices[0].message.content)
+        presentation = create_script(prompt, output)
 
-    with open(
-        os.path.join(output, "presentation.json"), "w", encoding="utf-8"
-    ) as file:
-        json.dump(presentation, file, indent=2)
+    with Progress() as progress:
+        task = progress.add_task(
+            "Creating slides...", total=2 * len(presentation)
+        )
 
-    with tqdm(total=len(presentation)) as progress:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for index, slide in enumerate(presentation):
+                futures.append(
+                    executor.submit(create_image, index, slide, output)
+                )
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    _ = future.result()
+                except Exception as exc:
+                    logger.error("Slide creation failed: %s", exc)
+                finally:
+                    progress.advance(task)
+
+        # TODO: Will have to make this concurrent too for speed
+        # FakeYou is kind of trash and needs to use serial requests
         for index, slide in enumerate(presentation):
-            progress.set_description(
-                f"Slide {index}: Image '{slide['image']}' ..."
-            )
-
-            response = openai.Image.create(
-                prompt=slide["image"],
-                n=1,
-                size="1024x1024",
-                api_key=OPENAI_API_KEY,
-            )
-            image_url = response["data"][0]["url"]
-
-            path = os.path.join(output, f"slide_{index:02d}.png")
-            urllib.request.urlretrieve(image_url, path)
-
-            progress.set_description(
-                f"Slide {index}: TTS ({speaker}) '{slide['text']}' ..."
-            )
-
-            path = os.path.join(output, f"slide_{index:02d}.wav")
-            fk_you.say(slide["text"], speaker).save(path)
-
-            progress.update(1)
+            create_audio(index, slide, output)
+            progress.advance(task)
 
 
 def slide_gen(prompt: str, output: str):
@@ -414,28 +512,24 @@ async def slide_gen_task(cmd: ChatCommand, output: str):
         await loop.run_in_executor(executor, slide_gen, cmd.parameter, output)
 
 
-async def run():
-    """Run the bot
+async def setup_obs(client: obs.ReqClient):
+    """Setup OBS
 
-    This function will create the bot and start it. The bot will join the
-    channel specified in the config and listen for commands. The bot will
-    respond to the `present` command. The bot will queue the presentation
-    requests and will start the presentation when the current presentation
-    finishes.
+    This function will setup OBS for the presentation. It will create a new
+    scene called `Presentation` and will add a VLC source called `vlc_source`
+    to the scene. It will also mute the `Mic/Aux` and `Desktop Audio` inputs.
+
+    Parameters
+    ----------
+    client : obs.ReqClient
+        The OBS client to use
     """
-    presentations = asyncio.Queue(maxsize=5)
-
-    client = obs.ReqClient(
-        host=OBS_WEBSOCKET_IP,
-        port=OBS_WEBSOCKET_PORT,
-        password=OBS_WEBSOCKET_PASSWORD,
-    )
-
     result = client.get_scene_list()
     scenes = result.scenes
     if "Presentation" in [scene["sceneName"] for scene in scenes]:
         client.remove_scene("Presentation")
 
+    # TODO: Can this be fixed?
     # Needed to wait for OBS to update the scene list... UGH!
     await asyncio.sleep(0.1)
 
@@ -453,10 +547,150 @@ async def run():
         True,
     )
 
+    client.create_input(
+        "Presentation",
+        "Text (FreeType 2)",
+        "text_ft2_source_v2",
+        {
+            "font": {
+                "face": "Sans Serif",
+                "flags": 0,
+                "size": 72,
+                "style": "",
+            },
+            "text": "",
+            "custom_width": 1920 * 2 / 3,
+            "word_wrap": True,
+        },
+        True,
+    )
+
     client.set_current_program_scene("Presentation")
+
+    response = client.get_scene_item_id("Presentation", "VLC Video Source")
+    item_id = response.scene_item_id
+    positionX = (1920 - 1024) / 2
+    positionY = (1080 - 1024) / 2
+    client.set_scene_item_transform(
+        "Presentation",
+        item_id,
+        {"positionX": positionX, "positionY": positionY},
+    )
 
     client.set_input_mute("Mic/Aux", True)
     client.set_input_mute("Desktop Audio", True)
+
+
+async def display_message(client: obs.ReqClient, message: str, duration: int):
+    """Display a message on the screen
+
+    This function will display a message on the screen. It will set the source
+    to be text and will set the text to the message. It will wait for the
+    duration and then will remove the source.
+
+    Parameters
+    ----------
+    client : obs.ReqClient
+        The OBS client to use
+    message : str
+        The message to display
+    duration : int
+        The duration to display the message for in seconds
+    """
+    client.set_input_settings(
+        "Text (FreeType 2)",
+        settings={
+            "text": message,
+        },
+        overlay=True,
+    )
+
+    await asyncio.sleep(0.1)
+
+    response = client.get_scene_item_id("Presentation", "Text (FreeType 2)")
+    item_id = response.scene_item_id
+    response = client.get_scene_item_transform("Presentation", item_id)
+    transform = response.scene_item_transform
+    source_width = transform["sourceWidth"]
+    source_height = transform["sourceHeight"]
+    positionX = (1920 - source_width) / 2
+    positionY = (1080 - source_height) / 2
+    client.set_scene_item_transform(
+        "Presentation",
+        item_id,
+        {"positionX": positionX, "positionY": positionY},
+    )
+
+    await asyncio.sleep(duration)
+
+    client.set_input_settings(
+        "Text (FreeType 2)",
+        settings={
+            "text": "",
+        },
+        overlay=True,
+    )
+
+
+async def play_run(client: obs.ReqClient, run: int):
+    """Play the video for the run
+
+    This function will play the video for the run. It will set the VLC source
+    to the video file and will wait for the video to finish.
+
+    Parameters
+    ----------
+    client : obs.ReqClient
+        The OBS client to use
+    run : int
+        The run number to play
+    """
+    path = os.path.join(OUTPUT, str(run), "prompt.txt")
+
+    with open(path, "r", encoding="utf-8") as file:
+        prompt = file.read()
+
+    await display_message(client, prompt, 3)
+
+    path = os.path.join(OUTPUT, str(run), "video.mp4")
+
+    client.set_input_settings(
+        "VLC Video Source",
+        settings={
+            "playlist": [
+                {
+                    "hidden": False,
+                    "selected": False,
+                    "value": path,
+                }
+            ],
+        },
+        overlay=True,
+    )
+
+    probe = ffmpeg.probe(path)
+    duration = float(probe["format"]["duration"])
+    await asyncio.sleep(duration)
+
+
+async def run_bot():
+    """Run the bot
+
+    This function will create the bot and start it. The bot will join the
+    channel specified in the config and listen for commands. The bot will
+    respond to the `present` command. The bot will queue the presentation
+    requests and will start the presentation when the current presentation
+    finishes.
+    """
+    presentations = asyncio.Queue(maxsize=5)
+
+    client = obs.ReqClient(
+        host=OBS_WEBSOCKET_IP,
+        port=OBS_WEBSOCKET_PORT,
+        password=OBS_WEBSOCKET_PASSWORD,
+    )
+
+    await setup_obs(client)
 
     async def on_ready(ready_event: EventData):
         await ready_event.chat.join_room(TARGET_CHANNEL)
@@ -468,7 +702,7 @@ async def run():
         elif presentations.full():
             await cmd.reply("Presentation queue is full, try again later!")
             logger.warning(
-                f"Presentation queue is full, {cmd.user.name} skipped"
+                "Presentation queue is full, %s skipped", cmd.user.name
             )
         else:
             run = get_output_run()
@@ -477,7 +711,9 @@ async def run():
                 "Presentation queued!"
                 f"{cmd.user.name} will present '{cmd.parameter}' next!"
             )
-            logger.info(f"Video queued for {cmd.user.name} with run {run}...")
+            logger.info(
+                "Video queued for %s with run %s...", cmd.user.name, run
+            )
 
             output = os.path.join(OUTPUT, str(run))
 
@@ -488,31 +724,26 @@ async def run():
         while True:
             run = await presentations.get()
 
-            logger.info(f"Starting presentation for run {run}...")
+            logger.info("Starting presentation for run %s...", run)
 
-            path = os.path.join(OUTPUT, str(run), "video.mp4")
-
-            client.set_input_settings(
-                "VLC Video Source",
-                settings={
-                    "playlist": [
-                        {
-                            "hidden": False,
-                            "selected": False,
-                            "value": path,
-                        }
-                    ],
-                },
-                overlay=True,
-            )
-
-            probe = ffmpeg.probe(path)
-            duration = float(probe["format"]["duration"])
-            await asyncio.sleep(duration)
+            await play_run(client, run)
 
             presentations.task_done()
 
+    async def random_presentation():
+        while True:
+            if presentations.empty():
+                run = get_random_run()
+
+                if run is not None:
+                    logger.info(
+                        "Starting random presentation for run %s...", run
+                    )
+
+                    await play_run(client, run)
+
     presentation_task = asyncio.create_task(presentation_task())
+    random_presentation_task = asyncio.create_task(random_presentation())
 
     twitch = await Twitch(APP_ID, APP_SECRET)
     auth = UserAuthenticator(twitch, USER_SCOPE)
@@ -536,7 +767,9 @@ async def run():
         chat.stop()
         await twitch.close()
         presentation_task.cancel()
+        random_presentation_task.cancel()
 
 
 def main():
-    asyncio.run(run())
+    """Entry point to the bot"""
+    asyncio.run(run_bot())
